ee1bc44f8766eaabb72fc21ef7d5cd277a113d92
Marcelo-Gzz <marcelogzzgarcia@gmail.com>
2024-10-26 12:35:30 -0500

    Hi

--- Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Release/net8.0/Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0517d6069d796dc5fde9d36e5f3cc3cf7b9afe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924c11db2a07cd3a8a1ce2c705c518ebea0ea94e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
