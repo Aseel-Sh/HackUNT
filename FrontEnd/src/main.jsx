7ae5a9e7bb2f8386a71a5cc0b8d117fa34aee800
juan correa <juancorrea50@yahoo.com>
2024-10-26 14:30:57 -0500

    Finish signIn logic

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './signIn.jsx'
 
 createRoot(document.getElementById('root')).render(
--- FrontEnd/src/signIn.jsx
+++ FrontEnd/src/signIn.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import './styling.css'
 
 
@@ -7,18 +6,48 @@ export default function signIn() {
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
 
-    function validate(pw,user){
-        
+    //Backend logic here
+
+    function validate(){
+        if(password.length < 6){
+            alert("Please enter a valid password" + ` ${password.length}`);
+        }
+        if(username.trim === ""){
+            alert("Please enter a valid username");
+        }
+        //Backend logic here
     }
 
     return(
         <div class="parent">
             <label>Sign In</label>
-            <label>Username:</label>
-            <input type='text' onChange={()=> setPassword(value)}/>
-            <label>Password:</label>
-            <input type='password'/>
-            <button>Submit</button>
+            <div>
+                <label>Username:</label>
+                <input
+                type='text'
+                style={{
+                    backgroundColor: "#FF8408",
+                    color: "#00FF37"
+                }}
+                onChange={() => {setUsername}}/>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input
+                style={{
+                    backgroundColor: "#00FF37",
+                    color: "#FF8408"
+                }}
+                type='password'
+                onChange={setPassword}/>
+            </div>
+
+            <button
+            style={{
+                backgroundColor: "#CD04FF"
+            }}
+            onClick={validate}
+            >Submit</button>
         </div>
     )
 }
