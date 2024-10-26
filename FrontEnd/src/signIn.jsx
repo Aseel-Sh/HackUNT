385b1774fa1928367fb612b5eaeb5cefbb39a8ec
juan correa <juancorrea50@yahoo.com>
2024-10-26 16:11:40 -0500

    Begin transferrence from css to nes-css

--- FrontEnd/src/signIn.jsx
+++ FrontEnd/src/signIn.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './styling.css'
-
+import 'nes.css/css/nes.min.css';
 
 export default function signIn() {
     const [username,setUsername] = useState('');
@@ -19,37 +19,92 @@ export default function signIn() {
     }
 
     return(
-        <div class="parent">
-            <div class='title-container'>
+        <div class="nes-constainer is-dark"
+        style={{
+            border: "0"
+        }}
+        >
+            <div class='nes-container is-dark'
+                style={{
+                fontSize: "2rem",
+                marginBottom: "2rem",
+                border: "0",
+                margin: "0"
+                }}
+            >
                 <label>Login</label>
             </div>
-            <div class="input-username"> 
+            <div class="nes-container is-dark"
+                style={{
+                    fontSize: "2rem",
+                    marginBottom: "2rem",
+                    border: "0",
+                    margin: "0"
+                }}
+            > 
                 <label>Username:</label>
                 <input
                 type='text'
+                class = 'input.is-dark'
                 style={{
                     backgroundColor: "#FF8408",
                     color: "#00FF37"
                 }}
                 onChange={() => {setUsername}}/>
             </div>
-            <div class='input-username'>
+            <div class='nes-container is-dark'
+                style={{
+                    fontSize: "2rem",
+                    marginBottom: "2rem",
+                    border: "0",
+                    margin: "0"
+                }}
+            >
                 <label>Password:</label>
                 <input
+                class="input.is-dark"
                 style={{
                     backgroundColor: "#00FF37",
-                    color: "#FF8408"
+                    color: "#FF8408",
+                    fontSize: "2rem",
+                    marginBottom: "2rem",
+                    border: "0",
+                    margin: "0"
                 }}
                 type='password'
                 onChange={setPassword}/>
             </div>
-
-            <button
+            {/*Remember sign in and forgot password */}
+            <label class='nes-container is-dark'
             style={{
-                backgroundColor: "#CD04FF"
+                margin: "0",
+                border: "0"
             }}
-            onClick={validate}
+            >
+                <input type='checkbox' class="nes-checkbox is-dark"/>
+                <span>Remember Me</span>
+            </label>
+            <div>
+                <a class = "nes-container is-dark" href='https://google.com'
+                    style={{
+                        margin: "0",
+                        border: "0"
+                    }}
+                >Forgor Password</a>
+            </div>
+            
+            <div
+                style={{
+                    margin: "0",
+                    border: "0"
+                }}
+            >
+            <button
+                class="nes-btn"
+                onClick={validate}
             >Login</button>
+
+            </div>
         </div>
     )
 }
