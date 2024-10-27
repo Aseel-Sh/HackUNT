d87bebffb7f2d3058f66ff3ede5022d915d723a0
juan correa <juancorrea50@yahoo.com>
2024-10-26 19:26:15 -0500

    Fix zIndex to allow clipping of webs through

--- FrontEnd/src/signIn.jsx
+++ FrontEnd/src/signIn.jsx
@@ -25,9 +25,8 @@ export default function signIn() {
             height: '100vh',
             width: '100vw',
             top: 0,
-            left: 0,    
-            transform: 'scale(.1)',
-            transform: 'scaleX(-1)',
+            left: 0,
+            transform: 'scale(-1, -1)',
             zIndex: 1
         }
       };
@@ -69,7 +68,8 @@ export default function signIn() {
                     fontSize: "2rem",
                     marginBottom: "2rem",
                     border: "0",
-                    margin: "0"
+                    margin: "0",
+                    zIndex: 3
                 }}
             > 
                 <label>Username:</label>
@@ -82,7 +82,8 @@ export default function signIn() {
                     fontSize: "2rem",
                     marginBottom: "2rem",
                     border: "0",
-                    margin: "0"
+                    margin: "0",
+                    zIndex: 3
                 }}
                 onChange={() => {setUsername}}/>
             </div>
@@ -91,7 +92,8 @@ export default function signIn() {
                     fontSize: "2rem",
                     marginBottom: "2rem",
                     border: "0",
-                    margin: "0"
+                    margin: "0",
+                    zIndex: 3
                 }}
             >
                 <label>Password:</label>
@@ -103,7 +105,8 @@ export default function signIn() {
                     fontSize: "2rem",
                     marginBottom: "2rem",
                     border: "0",
-                    margin: "0"
+                    margin: "0",
+                    zIndex: 3
                 }}
                 type='password'
                 onChange={setPassword}/>
@@ -133,17 +136,18 @@ export default function signIn() {
                     gap: "1rem"
                 }}
             >
-            <a href='https://google.com' style={{color:"white"}}>Forgor Password</a>
+            <a href='https://google.com' style={{color:"white", zIndex: 3}}>Forgor Password</a>
             <button
                 class="nes-btn"
                 onClick={validate}
                 style={{
                     backgroundColor: "#FF8408",
                     width: "60rem",
-                    padding: "0"
+                    padding: "0",
+                    zIndex: 3
                 }}
             >Login</button>
-            <label>Don't have an account? <a href='https://google.com' style={{color:'white'}}>Sign Up</a></label>
+            <label style={{zIndex: 3}}>Don't have an account? <a href='https://google.com' style={{color:'white', zIndex: 3}}>Sign Up</a></label>
             </div>
         </div>
     )
