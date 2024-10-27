afab1580bff02aa87497fa9f2a7a30e405db65c6
juan correa <juancorrea50@yahoo.com>
2024-10-27 02:05:52 -0500

    Revert to previous commit

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -6,25 +6,22 @@ import { createRoot } from 'react-dom/client'
 import App from './landingpage.jsx'
 import signIn from './signIn.jsx'
 import './styling.css'
+import LandingPage from './landingpage.jsx'
+import signUp from './signUp.jsx'
+import dashboard from './dashboard.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: 'signIn/',
-        element: <signIn />
-      }
-    ]
-  },
-  
-])
+const root = createRoot(document.getElementById("root"));
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
+root.render(
+  <BrowserRouter>
+    <Routes>
+      {/*Add new routes here for navigation */}
+      <Route path='/' Component={LandingPage}/>
+      <Route path='/signIn' Component={signIn}/>
+      <Route path='/signUp' Component={signUp}/>
+      <Route path='/signIn/dashboard' Component={dashboard}/>
+    </Routes>
+  </BrowserRouter>
 )
